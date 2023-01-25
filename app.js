49a18c4a6441e748c5faa34ed13f9934ce64ac4f
Lindsay M <lminami23@punahou.edu>
2023-01-24 16:39:00 -1000

    update for 2023

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var messageInput = document.getElementById("message");
-var requestedTime = document.getElementById("requested");
 
 function subForm(){
 	// console.log(messageInput.value)
 	var today = new Date();
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-	// console.log(date + " " + time);
+	console.log(date + " " + time);
 	
-	fetch("https://api.apispreadsheets.com/data/[apiKey]/", {
+	fetch("https://api.apispreadsheets.com/data/oLKCVdRUHd6Yl5Hx/", {
 		method: "POST",
-		body: JSON.stringify({"data": {"time":date + " " + time,"message":messageInput.value,"requested":requestedTime.value}}),
+		body: JSON.stringify({"data": {"time":date + " " + time,"message":messageInput.value}}),
 	}).then(res =>{
 		if (res.status === 201){
 			// console.log("worked!")
 			messageInput.value = ""
-			requestedTime.value = ""
+		}
+		else {
+			console.log('test')
 		}
 	});
 }
